aa553eac109fc357a8bfe55d50de662be13713a6
Ramshley P. <ramshleyv@gmail.com>
2025-07-06 20:26:22 -0400

    Add project files.

---
+++ P0008/Models/Tipos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P0008.Models
+{
+	public class Tipos
+	{
+        public int IdTipos { get; set; }
+        public string Descripcion { get; set; }
+        public string Sigla { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
