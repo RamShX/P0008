032a7bb2265bcaabd6686afacf48062d7d2015ef
Ramshley P. <ramshleyv@gmail.com>
2025-07-06 20:28:03 -0400

    Primer commit

--- P0008/Models/Tipos.cs
+++ P0008/Models/Tipos.cs
@@ -9,7 +9,7 @@ namespace P0008.Models
 	{
         public int IdTipos { get; set; }
         public string Descripcion { get; set; }
-        public string Sigla { get; set; }
+        public byte[] Imagen { get; set; }
         public bool Activo { get; set; }
 
     }
